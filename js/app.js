c3df1e6e821cfc869283ea6cf73c40d33ffe82ae
Steven Cable <steven.cable12@gmail.com>
2017-02-18 11:05:41 -1000

    working functions need to add bonus still. can/should also refactor to a single reduce

--- js/app.js
+++ js/app.js
@@ -1,2 +1,46 @@
+// let score = (
+let arr = [-2,6,11,'steven'];
+  function getValidNumbers(element, index, array){
+    if(typeof element !== 'number'){
+      return false;
+    }else if(element>=0){
+      return parseInt(element); 
+    }else{
+      return false;
+    }
+  }
+  let stuff = arr.filter(getValidNumbers)
+  console.log(stuff)
 
+  function totalScore(prev, curr, index, array){
+      return prev + curr;
+    }
+
+  // function checkAll(element){
+  //   return
+  // } NEED TO ADD BONUS WITH THIS CHECK
+
+  function scoreThrows(array){
+
+   return  array.filter(getValidNumbers)
+    .map((element, index, array) => {
+      if(element>0 && element<5){
+        return 10;
+      }
+      if(element>=5 && element <=10){
+        return 5;
+      }
+      if(element>10){
+        return 0
+      }
+    })
+    .reduce(totalScore,0)
+
+    }
+
+// )
+
+console.log('score should be 15: ',scoreThrows([-1,2]))
+
+// module.exports = score; 
 
--- js/test.js
+++ js/test.js
@@ -1,5 +1,21 @@
 const chai = require('chai')
 chai.should();
 
-const 
+const scoreThrows = require('./app.js');
+
+let correctArray = [0,5,10,11]; //20
+let negativeArray = [-1,5,10,11]; //err
+let nonNumberArray = ['three', 5,10,11]; //err
+
+describe('Dart Score!', _=>{
+  describe('Score: ', _=>{
+
+    it('should be an array of numbers', _()=>{
+      scoreThrows.correctArray
+    })
+
+  })
+
+
+})
 
